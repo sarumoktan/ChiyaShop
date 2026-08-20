e72bc0c219a765a3def172c00e5c76f6d1ff2f80
sarumoktan <sarumoktan198@gmail.com>
2026-08-20 22:54:15 +0545

    api

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,29 @@
+/** An error with an HTTP status code, thrown by controllers and handled centrally. */
+class ApiError extends Error {
+  constructor(statusCode, message, details = null) {
+    super(message);
+    this.name = 'ApiError';
+    this.statusCode = statusCode;
+    this.details = details;
+    this.isOperational = true;
+    Error.captureStackTrace?.(this, ApiError);
+  }
+
+  static badRequest(message = 'Bad request', details) {
+    return new ApiError(400, message, details);
+  }
+  static unauthorized(message = 'You need to sign in to continue.') {
+    return new ApiError(401, message);
+  }
+  static forbidden(message = 'You do not have access to this resource.') {
+    return new ApiError(403, message);
+  }
+  static notFound(message = 'Resource not found.') {
+    return new ApiError(404, message);
+  }
+  static conflict(message = 'That already exists.') {
+    return new ApiError(409, message);
+  }
+}
+
+module.exports = ApiError;
